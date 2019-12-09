3917c9cff14c0c3556bd6c706940f60e26d6efe4
swift260919 <swift260919@gmail.com>
2019-12-09 09:24:53 +0200

    lec16 started

---
+++ Lec16/Lec16/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lec16
+//
+//  Created by HACKERU on 09/12/2019.
+//  Copyright © 2019 HACKERU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
