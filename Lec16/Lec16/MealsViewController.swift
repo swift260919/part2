32a34446e67290e089e1d6ff5c08d848d280580c
swift260919 <swift260919@gmail.com>
2019-12-09 12:09:13 +0200

    lec16 started

--- Lec16/Lec16/MealsViewController.swift
+++ Lec16/Lec16/MealsViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 import CoreData
 
+
+//DAO
+//getCurrentUser
+//login
+//register
+//get meals for user
+//add meal to current user
+//edit meals for current user
+
 class MealsViewController: UIViewController {
     @IBOutlet weak var personNameLabel: UILabel!
     
@@ -56,12 +65,72 @@ class MealsViewController: UIViewController {
         }
     }
     
+    //when we perform the segue -> we pass the person for editing.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let dest = segue.destination as? EditPersonViewController,
+           let sender = sender as? Person{
+           
+            dest.currentPerson = sender
+        }
+    }
+    
     @IBAction func addMeal(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(
+            title: "Add a meal",
+            message: "Describe your meal",
+            preferredStyle: .alert
+        )
+        
+        //add a text field
+        alert.addTextField { (textField) in
+            textField.placeholder = "Meal Name"
+            textField.keyboardType = .alphabet
+        }
+        //add an action
+        alert.addAction(UIAlertAction(title: "Add", style: .default, handler: { (action) in
+            //create a meal
+            let meal = Food(context: CoreDataStack.shared.context)
+            meal.name = alert.textFields?[0].text
+            meal.date = Date()
+            
+            //add the meal to our current person:
+            //var copy = self.currentPerson?.meals?.mutableCopy() as? NSMutableOrderedSet
+            //copy?.add(meal)
+            //self.currentPerson?.meals = copy
+            
+            self.currentPerson?.addToMeals(meal)
+            
+            //save the new meal
+            CoreDataStack.shared.saveContext()
+            //reload the table view
+            self.mealsTableView.reloadData() //prefer reloadRowsAt
+        }))
+        
+        //show the alert
+        present(alert, animated: true)
     }
     
     @IBAction func editPerson(_ sender: UILongPressGestureRecognizer) {
-        
-        performSegue(withIdentifier: "editPersonSegue", sender: nil)
+        performSegue(withIdentifier: "editPersonSegue", sender: currentPerson)
     }
 }
 
+
+
+extension MealsViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return currentPerson?.meals?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        //bind the meal data to the cell
+        if let meal = currentPerson?.meals?.object(at: indexPath.row) as? Food{
+            cell.textLabel?.text = "\(meal.name ?? "") - \(meal.date ?? Date())"
+        }
+        
+        return cell
+    }
+}
